c8f46a0719d749b0777729fa252d45f0aabd43f7
Shani Tiwari <shanitiwari3354@gmail.com>
2026-09-17 23:17:26 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# Vercel build step: install dependencies and collect static files.
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+set -e
+
+echo "Installing dependencies..."
+python3 -m pip install --break-system-packages -r requirements.txt \
+  || python3 -m pip install -r requirements.txt
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear
