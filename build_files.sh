fcc07fee4148c9e79e16f86719f6addbd2a70fdf
Shani Tiwari <shanitiwari3354@gmail.com>
2026-09-17 23:20:17 +0530

    Delete build_files.sh

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing dependencies..."
-python3 -m pip install --break-system-packages -r requirements.txt \
-  || python3 -m pip install -r requirements.txt
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
